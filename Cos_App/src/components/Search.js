48fee279b9bc672143077dbb34543aa34aa4fc7a
Souptik72 <datta.souptik@yahoo.in>
2018-06-12 16:32:27 +0530

    added search bar input and button in search.js

--- Cos_App/src/components/Search.js
+++ Cos_App/src/components/Search.js
@@ -3,10 +3,20 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  View
 } from 'react-native';
 
 export default class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+       
+    }
+  }
   static navigationOptions = {
     title: 'Search',
     headerStyle: {
@@ -23,8 +33,23 @@ export default class SearchBar extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is text input</Text>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={{height: 40, width: 200, borderWidth: 2, borderColor: 'gray',}}
+          placeholder='search here...'
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+          editable={true}
+          underlineColorAndroid='transparent'
+        />
+        <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text> Search </Text>
+       </TouchableOpacity>
       </View>
+        </View>
     );
   }
 }
@@ -35,5 +60,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  }
+  },
+ searchContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  },
+ 
 });
