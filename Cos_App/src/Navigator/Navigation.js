f461ed6ae124dba1216731a832e89c404b3aac34
rajat-jain9 <jainrajatcse@gmail.com>
2018-06-18 13:25:42 +0530

    removed ContentViewer Page

--- Cos_App/src/Navigator/Navigation.js
+++ Cos_App/src/Navigator/Navigation.js
@@ -13,12 +13,10 @@ import {
 import Authentication from '../components/Authentication';
 import SearchBar from '../components/Search';
 import SearchList from '../components/SearchList';
-import ContentViewer from '../components/ContentViewer';
 
 const Navigation = StackNavigator({
   SearchPage: { screen: SearchBar }, 
   SearchListPage: { screen: SearchList},
-  ContentPage: { screen: ContentViewer},
   AuthPage: { screen: Authentication },
 });
 
